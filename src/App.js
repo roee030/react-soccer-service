523874025d9ae3e2aa2c54b8b0f8b6b03d0d77f1
roee030 <roee.angel93@gmail.com>
2019-10-16 23:10:43 +0300

    use promise

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React from 'react';
 import './App.css';
 import data_file from './result_played.csv'
 import {csv} from "d3"
-
-
+ 
 
 function App() {
+  var data_results = [];
   const results_promise = new Promise((resolve,reject)=>{
     resolve(csv(data_file))
   })
   results_promise.then((results)=>{
-    console.log("success",results)
+    for(var i = 0 ; i < results.length ; i++)
+    {
+      if(results[i].home_score == 0)
+      {
+        console.log("אני גאון")
+      }
+    }
   })
+  
+
+
+
     return (
     <div className="App">
-
+   
     </div>
   );
 }
