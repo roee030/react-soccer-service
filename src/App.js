447686ac3bd77c0dd479f39f2235ae3981b1d979
roee030 <roee.angel93@gmail.com>
2019-10-19 04:48:22 +0300

    clear the dead code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import SearchBar from 'react-js-search';
 import AutoComplete from './components/AutoComplete';
-var data1 = require('./get_Data')
-var data2 = require ('./result_played.csv')
-var helper = require('./helper')
 
 export default class App extends Component {
   constructor(props){
@@ -13,8 +9,6 @@ export default class App extends Component {
       soccer_teams: data1.total_team,
       tournaments: data1.total_tournament
     }
-    const rakrega = helper._inputs
-    console.log(rakrega)
     
   }
  
