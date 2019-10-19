4eec3826c38a22106b5518fb31215d4386ebf8e3
roee030 <roee.angel93@gmail.com>
2019-10-19 17:44:00 +0300

    organize the data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react'
 import './App.css';
 import AutoComplete from './components/AutoComplete';
+var fs = require('fs'); 
+
 
 export default class App extends Component {
   constructor(props){
     super(props)
     this.state = {
       soccer_teams: [],
-      tournaments: []
+      tournaments: [],
     }
-    
+   
+   
   }
- 
+
 
   render() {
     return (
       <div>
-        
+      {
+        this.state.data.map((value) =>
+        <div>
+          <span>{value.home_team}</span>
+         
+        </div>
+        )
+      } 
       </div>
     )
   }
---
+++ src/helper2.js
@@ -0,0 +1,19 @@
+var fs = require('fs'); 
+var parse = require('csv-parse');
+
+var csvData=[];
+fs.createReadStream('./result_upcoming.csv')
+    .pipe(parse({delimiter: ':',relax_column_count: true,
+}))
+    .on('data', function(csvrow) {
+        //console.log(csvrow);
+        //do something with csvrow
+        csvData.push(csvrow)
+    })
+    .on('end',function() {
+      //do something wiht csvData
+      const dataJSON = JSON.stringify(csvData)
+
+        fs.writeFileSync('result_upcoming.json',dataJSON)
+    });
+
