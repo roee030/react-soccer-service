e204d54c2549db326c4fc7d910c5b1d8304cb5a2
roee030 <roee.angel93@gmail.com>
2019-10-19 04:49:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      soccer_teams: data1.total_team,
-      tournaments: data1.total_tournament
+      soccer_teams: [],
+      tournaments: []
     }
     
   }
